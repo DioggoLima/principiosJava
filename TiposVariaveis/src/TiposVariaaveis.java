fdd762745710d6d2022f52c34c36c81c433abbf7
DioggoLima <dioggolima77@gmail.com>
2025-02-12 13:46:07 -0300

    Alterações

--- TiposVariaveis/src/TiposVariaaveis.java
+++ TiposVariaveis/src/TiposVariaaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade= 39;
+        short ano = 2025;
+        int cep = 12345678;
+        long cpf = 12345678901L;
+        float salario = 1999.99f;
+        double salarioAnual = 23999.88;
+
+        final double PI = 3.14159265359;
+        System.out.println(PI);
     }
 }
